c444d57b104c7a7f7cd33494d2a12bae0be88c84
RicardoCarrillp <carrilloricki2211@gmail.com>
2023-08-21 12:33:51 -0500

    feat:download csv to test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {CookieService} from "ngx-cookie-service";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import {CookieService} from "ngx-cookie-service";
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule
   ],
   providers: [CookieService],
   bootstrap: [AppComponent]
